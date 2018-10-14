3b0e5fbdd08b6ba72e1d44ca53bf1523c895bf91
Diego Rodriguez <diego.ba.rodriguez@gmail.com>
2018-10-14 12:23:04 -0300

    Reperando error con jquery

--- beta/index.php
+++ beta/index.php
@@ -7,8 +7,8 @@
 <head>
 	<title></title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-	<script type="text/javascript" src="/arrowchat/includes/js/jquery.js"></script>
-	<!--<script src="includes/js/jquery.js"></script>-->
+	<!--<script type="text/javascript" src="/arrowchat/includes/js/jquery.js"></script>-->
+	<script src="includes/js/jquery.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="includes/css/sidebarNavigation.css">
