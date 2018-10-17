a8a7a6ef8fb6794759cee6c2f551a5dde41cd042
Diego Rodriguez <diego.ba.rodriguez@gmail.com>
2018-10-17 16:31:46 -0300

    Fixes en la traduccion
    1- El acento no aparecia correctamente en el boton flotante de chat.
    2-Al ingresar al chat movil, se detectaba el idioma ingles (dando opcion
    a traductor de google) debido a que lenguaje de la pagina estaba seteado
    en ingles.

--- beta/arrowchat/public/mobile/index.php
+++ beta/arrowchat/public/mobile/index.php
@@ -20,7 +20,7 @@
 		$home_url = "../../";
 ?>
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es" lang="es">
 	<head>
 		<title>Mobile Chat</title>
 		
